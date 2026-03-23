653944a96fb4fe455c28742afc57b17e14b22e3f
CamilaGonzalezBrito <115099889+CamilaGonzalezBrito@users.noreply.github.com>
2026-03-23 09:39:24 +0000

    actualizo codigo

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
+const pronouns = ['the', 'our'];
+const adjectives = ['great', 'big'];
+const nouns = ['jogger', 'racoon'];
+const tlds = [".com", ".net", ".us", ".io"];
 
-
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let tlds = [".com", ".net", ".us", ".io"]
-
-for(let i = 0; i < pronoun.length; i++){
-  for(let a = 0; a < adj.length; a++){
-    for(let n = 0; n < noun.length; n++){
-      for(let t = 0; t < tlds.length; t++){
-        console.log(pronoun[i] + adj[a] + noun[n] + tlds[t]);
+/*
+Actualización del proyecto generador de dominios.
+Se organizaron mejor los arrays y se añadieron pequeños
+ajustes para mejorar la claridad del código.
+El script genera posibles nombres de dominio combinando
+pronombres, adjetivos, sustantivos y extensiones,
+mostrando todas las combinaciones en la consola.
+*/
+for (let i = 0; i < pronouns.length; i++) {
+  for (let a = 0; a < adjectives.length; a++) {
+    for (let n = 0; n < nouns.length; n++) {
+      for (let t = 0; t < tlds.length; t++) {
+        console.log(pronouns[i] + adjectives[a] + nouns[n] + tlds[t]);
       }
     }
   }
